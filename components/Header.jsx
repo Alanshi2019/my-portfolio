2b396ce9f091470ac1cfbeccaecf234dc352a074
Alanshi2019 <alanwork94@gmail.com>
2024-08-13 11:00:37 -0400

    Page & Stair Transition

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,21 +3,21 @@ import { Button } from "./ui/button";
 import Nav from "./Nav";
 
 const Header = () => {
-  return (<header className="py-8 xl:py-12 text-white bg-pink-50/20">
+  return (<header className="py-8 xl:py-12 text-white">
     <div className="container mx-auto flex justify-between items-center">
       <Link href="/">
         <h1 className="text-4xl font-semibold">Alan <span className="text-accent">.</span></h1>
       </Link>
 
       <div className="hidden xl:flex items-center gap-8">
-       <Nav />
-       <Link href="/contact">
-         <Button>Hire me</Button>
-       </Link>
+        <Nav />
+        <Link href="/contact">
+          <Button>Hire me</Button>
+        </Link>
       </div>
 
       <div className="xl:hidden">mobile nav</div>
-      
+
     </div>
   </header>)
 }
