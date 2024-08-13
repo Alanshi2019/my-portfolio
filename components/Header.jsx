dd46096204b4c95c0f5947c2a2263f6713d261d3
Alanshi2019 <alanwork94@gmail.com>
2024-08-13 14:10:20 -0400

    Count up

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (<header className="py-8 xl:py-12 text-white">
     <div className="container mx-auto flex justify-between items-center">
       <Link href="/">
-        <h1 className="text-4xl font-semibold">Alan <span className="text-accent">.</span></h1>
+        <h1 className="text-4xl font-semibold">Alan<span className="text-accent">.</span></h1>
       </Link>
 
       <div className="hidden xl:flex items-center gap-8">
--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -41,7 +41,7 @@ const MobileNav = () => {
     <SheetContent className="flex flex-col">
       <div className="=mt-32 mb-40 text-center text-2xl">
         <Link href="/">
-          <h1 className="text-4xl font-semibold">Alan <span className="text-accent">.</span></h1>
+          <h1 className="text-4xl font-semibold">Alan<span className="text-accent">.</span></h1>
         </Link>
       </div>
       <nav className="flex flex-col justify-center items-center gap-8">{links.map((link, index) =>
